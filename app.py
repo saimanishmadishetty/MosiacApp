2c6e74defedd47cb7054d88b7b65c7890bcc0e4f
saimanishmadishetty <168367986+saimanishmadishetty@users.noreply.github.com>
2024-09-04 14:37:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from vipas import model
 from vipas.exceptions import UnauthorizedException, NotFoundException
 
 def predict_image(input_data):
-    model_id = "mdl-ac1l5kuvbhbp3"
+    model_id = "mdl-lq0uiy2f79fto"
     vps_model_client = model.ModelClient()
     response = vps_model_client.predict(model_id=model_id, input_data=input_data)
     return response
